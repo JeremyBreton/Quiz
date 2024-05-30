28eef09e8657535cf69fe42d6d5d11eaf8142208
Jérémy <breton.jeremy@live.fr>
2024-05-30 16:00:26 +0200

    Add Swagger doc

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -6,6 +6,15 @@ const NoResourceFoundError = require('../../errors/NoResourceFoundError');
 
 const router = express.Router();
 
+/**
+ * GET /api
+ *
+ * @summary get API documentation URL
+ * @tags Docs - The blog's API documentation
+ *
+ * @return {object} 200 - success response
+ */
+
 router.use('/quiz', quizRouter);
 router.use('/question', questionRouter);
 
--- app/routers/api/quiz.js
+++ app/routers/api/quiz.js
@@ -5,9 +5,40 @@ const { post, patch } = require('../../validations/schemas/quiz.schema')
 
 const router = express.Router();
 
+/**
+ * a quiz type
+ *
+ * @typedef {object} Quiz
+ * @property {number} id - quiz id
+ * @property {string} quiz_name - quiz name
+
+ */
+
+/**
+ * GET /api/quiz
+ *
+ * @summary get all quiz
+ * @tags Quiz - All quiz in the app
+ *
+ * @return {array<Quiz>} 200 - success response
+ * @return {object} 500 - internal server error
+ */
+
 // router.get('/', quizController.getAll);
 // On ajoute bind car sinon this est undefined. Ici avec bind on dit que this doit suivre quizController
 router.get('/', quizController.getAll.bind(quizController));
+
+/**
+ * POST /api/quiz
+ *
+ * @summary Create a new quiz
+ * @tags Quiz
+ *
+ * @param {Quiz} request.body - Quiz
+ *
+ * @return {Quiz} 200 - success response
+ * @return {object} 500 - internal server error
+ */
 router.post('/', validate(post, 'body'),quizController.create.bind(quizController));
 router.get('/:id([0-9]*)', quizController.getOne.bind(quizController));
 router.patch('/:id([0-9]*)', validate(patch, 'body'),quizController.modify.bind(quizController));
